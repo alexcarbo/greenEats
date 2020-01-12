30a05d260714b16d6345885ec2d45edc784745d4
Richard Echegaray <richardechegaray@outlook.com>
2020-01-11 17:24:26 -0800

    added pages that u click onto

--- nwHacks2020/app/src/main/java/com/example/nwhacks2020/MainMenu.java
+++ nwHacks2020/app/src/main/java/com/example/nwhacks2020/MainMenu.java
@@ -2,7 +2,9 @@ package com.example.nwhacks2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void goToInventory(View view){
+        Intent intent = new Intent(getApplicationContext(), Inventory.class);
+        startActivity(intent);
+    }
+
+    public void goToAddFood(View view){
+        Intent intent = new Intent(getApplicationContext(), AddFood.class);
+        startActivity(intent);
+    }
+
+    public void goToBuyFood(View view){
+        Intent intent = new Intent(getApplicationContext(), BuyFood.class);
+        startActivity(intent);
     }
 }
