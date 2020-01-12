bc5ea2947a484f6944b5a23eddb1eda52a6066cd
alexcarbo <alexcarbo6@gmail.com>
2020-01-11 20:03:58 -0800

    Can take Pictures

---
+++ nwHacks2020/app/src/main/java/com/example/nwhacks2020/Camera.kt
@@ -0,0 +1,12 @@
+package com.example.nwhacks2020
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Camera : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
