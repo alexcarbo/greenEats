00693165ce0bcbdc6f608579cd0b68ca2a437859
Richard Echegaray <richardechegaray@outlook.com>
2020-01-11 23:26:30 -0800

    can parse voice now

--- nwHacks2020/app/src/main/java/com/example/nwhacks2020/AddFood.java
+++ nwHacks2020/app/src/main/java/com/example/nwhacks2020/AddFood.java
@@ -2,7 +2,9 @@ package com.example.nwhacks2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddFood extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class AddFood extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_food);
+    }
+
+    public void goToSpeech(View view){
+        Intent intent = new Intent(getApplicationContext(), Speech.class);
+        startActivity(intent);
     }
 }
